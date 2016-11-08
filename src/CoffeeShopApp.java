2c862c0417efa8c945baa6eeecfa5dbde7bf8468
Manjula Kaza <mangoyum@gmail.com>
2016-11-08 10:58:17 -0500

    Fixed merge conflicts

--- src/CoffeeShopApp.java
+++ src/CoffeeShopApp.java
@@ -1,34 +1,10 @@
-<<<<<<< HEAD
-import java.util.Scanner;
 
-||||||| merged common ancestors
 
-=======
->>>>>>> 406d8ada2fbf10f2fc463640c92e30b88596d813
+
 public class CoffeeShopApp {
-	Scanner scan = new Scanner (System.in);
+	
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		
-		Product p = new Product ("rooibos tea", "beverage", "African herbal red tea", 2.95);
-		Product q = new Product ("chai tea", "beverage", "aromatic black tea with cardamom and other spices", 2.95);
-		
-		System.out.println((p.getPrice()) + q.getPrice());
-		
-		
-		
-||||||| merged common ancestors
-
-=======
->>>>>>> 406d8ada2fbf10f2fc463640c92e30b88596d813
 	}
-	public String paymentChoice(String paymentType){
 		
-		if (paymentType.equalsIgnoreCase("cash")){
-			
-		}
-		return null;
-	}
-	
 }
--- src/Payment.java
+++ src/Payment.java
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
 
 public abstract class Payment {
-||||||| merged common ancestors
 
-public class Payment {
-=======
-public abstract class Payment {
-	
->>>>>>> 406d8ada2fbf10f2fc463640c92e30b88596d813
 	private double subtotal;
 	private final double TAX = 1.06;
 	private double grandTotal;
-	
-	//constructor
+
+	// constructor
 	public Payment(double subtotal) {
 		super();
 		this.subtotal = subtotal;
-<<<<<<< HEAD
-		
-			
-||||||| merged common ancestors
-		this.TAX = tax;
-		this.grandTotal = grandTotal;	
-=======
->>>>>>> 406d8ada2fbf10f2fc463640c92e30b88596d813
+
 	}
-//getters and setters
+
+	// getters and setters
 	public double getSubtotal() {
 		return subtotal;
 	}
@@ -37,26 +24,16 @@ public abstract class Payment {
 	public void setSubtotal(double subtotal) {
 		this.subtotal = subtotal;
 	}
-<<<<<<< HEAD
-
-	public void generateGrandTotal() {
-		grandTotal = subtotal * TAX;
-	}
-	
-||||||| merged common ancestors
 
 	public void setGrandTotal(double grandTotal) {
 		this.grandTotal = grandTotal;
 	}
-	
-=======
->>>>>>> 406d8ada2fbf10f2fc463640c92e30b88596d813
-	
+
 	public double generateGrandTotal() {
 		grandTotal = subtotal * TAX;
-		
+
 		return grandTotal;
-		
+
 	}
-	
+
 }
